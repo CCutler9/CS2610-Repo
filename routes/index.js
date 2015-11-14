6c0bb552ff2e433fcd376d6792d3997072443d6a
Derek Clark <d_edward@live.com>
2015-11-14 09:16:16 -0700

    modified:   app.js 	moved routes to respective route folders
    	'/' moved to routes/index
    	'/profile' '/dashboard' and '/search' moved to routes/user
    
    	modified:   package.json
    	added required npm
    
    	modified:   public/css/Derek.css
    	made necessary changes for search and index pages
    
    	modified:   routes/user.js
    	added get request to '/search'
    
    	modified:   views/index.handlebars
    	modified log-in div and added profile div
    
    	modified:   views/layouts/auth_base.handlebars
    	modified:   views/layouts/base.handlebars
    	updated links in menu and modified title
    
    	modified:   views/search.handlebars
    	updated form and search results

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,57 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var request = require('request')
+var querystring = require('querystring')
+var cfg = require('../config')
 
 router.get('/', function(req, res) {
+  res.render('index',{
+    user: req.session.user,
+    title: 'Home'
+  })
+})
 
-  res.render('index', {
-    title: 'This is a title!',
-    welcome: 'Welcome to the site!'
+router.get('/authorize', function (req, res) {
+  var qs = {
+    client_id: cfg.client_id,
+    redirect_uri: cfg.redirect_uri,
+    response_type: 'code'
+  }
+  var query = querystring.stringify(qs)
+  var url = 'https://api.instagram.com/oauth/authorize/?' + query
+
+  res.redirect(url)
+})
+
+router.get('/auth/finalize', function(req, res, next) {
+  if(req.query.error == 'access_denied') {
+    return res.redirect('/')
+  }
+
+  var post_data = {
+    client_id: cfg.client_id,
+    client_secret: cfg.client_secret,
+    redirect_uri: cfg.redirect_uri,
+    grant_type: 'authorization_code',
+    code: req.query.code
+  }
+
+  var options = {
+    url: 'https://api.instagram.com/oauth/access_token',
+    form: post_data
+  }
+
+  request.post(options, function(error, response, body) {
+    try {
+      var data = JSON.parse(body)
+    }
+    catch(err){
+      return next(err)
+    }
+
+    req.session.access_token = data.access_token
+		req.session.user = data.user
+    res.redirect('/feed')
   })
 })
 
